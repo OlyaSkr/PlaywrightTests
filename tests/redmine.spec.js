d284e443c12567a486676e6c5618a678491e374e
Olha Skoryk <olha.skoryk.qaengineer@gmail.com>
2022-09-17 21:14:22 +0300

    results

--- tests/redmine.spec.js
+++ tests/redmine.spec.js
@@ -24,41 +24,24 @@ test.describe("redmine page", () => {
         await homePage.goto("https://www.redmine.org/");
     });
     test("sign up to redmine", async ({ page }) => {
-        // click on the sign up button
         await homePage.signUpClick();
-        // enter username in the username field;
         await signUpPage.usernameInputFill("New_testing_account");
-        // enter password in the password field
         await signUpPage.passwordInputFill("rtnk");
-        // enter confirmation password in the password field
         await signUpPage.passwordConfirmationInputFill("rtnk");
-        // enter firstname in the input field
         await signUpPage.userFirstnameInputFill("User");
-        // enter lastname in the input field
         await signUpPage.userLastnameInputFill("Testing");
-        // enter email in the input field
         await signUpPage.userEmailInputFill("newtestingaccount@ukr.net");
-        // clicl on the submit button
         await signUpPage.submitButtonClick();
-        //  await loginPage.pause(4000);
-        // check that successful registration message is enabled
         await loginPage.flashNoticeIsVisible();
-        // await expect(page.locator('.view label')).toHaveTitle('Redmine');
     });
     test("sign in to redmine", async ({ page }) => {
-        // click on the sign in button
         await homePage.signInClick();
-        // enter user name in the username field;
         await loginPage.usernameInputFill("Test__account");
-        // enter password in the password field
         await loginPage.passwordInputFill("er23kl");
-        // click on the submit button
         await loginPage.submitButtonClick();
-        // check that the inscription on the site 'my account' is visible
         await homePage.accountNameEnabled();
     });
     test("search in redmine from home page", async ({ page }) => {
-        // add news to the search field
         await homePage.searchInputFill("news");
         await page.keyboard.press("Enter");
         await expect(page).toHaveTitle("Search - Redmine");
@@ -69,20 +52,15 @@ test.describe("redmine page", () => {
         const [download] = await Promise.all([
             page.waitForEvent("download"),
             await downloadPage.redmineLinkClick(),
-            //downloadPage.locator('[href*="redmine-4.2.7.tar.gz"]').click(),
         ]);
         const suggestedFileName = download.suggestedFilename();
         const filePath = "download/" + suggestedFileName;
         await download.saveAs(filePath);
     });
     test("brief description of all issues", async ({ page }) => {
-        // click on the issues link
         await homePage.issuesLinkClick();
-        // click on the brief description link
         await issuesPage.issuesLinkClick();
-        // check that reports are visible
         await redminePage.reportLabelIsVisible();
-        // check page title
         await expect(page).toHaveTitle("Redmine");
     });
 });
